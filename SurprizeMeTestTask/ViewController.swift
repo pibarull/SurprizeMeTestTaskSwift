5ff05f8f04a8f4d8f8ba721c12dd515f36a738d8
Ershov Ilia <il998x@gmail.com>
2019-10-28 17:43:24 +0300

    Initial Commit

---
+++ SurprizeMeTestTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SurprizeMeTestTask
+//
+//  Created by Илья Ершов on 28/10/2019.
+//  Copyright © 2019 Ilia Ershov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
