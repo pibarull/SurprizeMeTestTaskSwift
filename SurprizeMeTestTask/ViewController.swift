26d74331a9b8f7be4c0ab760c7c657cb6d974322
Ershov Ilia <il998x@gmail.com>
2019-11-01 11:17:01 +0300

    Location switch fixed

--- SurprizeMeTestTask/ViewController.swift
+++ SurprizeMeTestTask/ViewController.swift
@@ -10,73 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - Variables
+    var userData: UserData?
+    
+    //MARK: - Outlets
     @IBOutlet weak var avatarImage: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
-
-    
-    var userData: UserData?
-    private var imageURL: String?
-    
+    //MARK: - Functions
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-
         
         activityIndicator.isHidden = true
         activityIndicator.hidesWhenStopped = true
         
         fetchData()
-        
-        
     }
 
-    @IBAction func rateUsButton(_ sender: Any) {
-        
-        let urlStr = "https://itunes.apple.com/app/id1054189818?action=write-review"
-        guard let url = URL(string: urlStr), UIApplication.shared.canOpenURL(url) else { return }
-        UIApplication.shared.open(url, options: [:], completionHandler: nil)
-    }
-    
-    @IBAction func ourPartnersButton(_ sender: Any) {
-        
-        guard let url = URL(string: "https://surprizeme.ru") else  { return }
-        UIApplication.shared.open(url, options: [:], completionHandler: nil)
-    }
-    
-    @IBAction func legalButton(_ sender: Any) {
-        guard let url = URL(string: "https://srprsm.com/contacts/") else  { return }
-        UIApplication.shared.open(url, options: [:], completionHandler: nil)
-    }
-    
-    
-    
-    @IBAction func mailClientButton(_ sender: Any) {
-        
-        guard let url = URL(string: "mailto:partners@surprizeme.ru") else  { return }
-        UIApplication.shared.open(url, options: [:], completionHandler: nil)
-    }
-    
     func fetchData() {
         
         activityIndicator.isHidden = false
         activityIndicator.startAnimating()
         
         guard let url = URL(string: "https://app.surprizeme.ru/api/profile/") else { return }
-        
         var request = URLRequest(url: url)
         
         request.setValue("Token b08ec419bc777ba24264ec5cd426b874c89e4c34", forHTTPHeaderField: "Authorization")
-    
+        
         URLSession.shared.dataTask(with: request) { (data, response, error) in
-            guard let data = data, let response = response else { return }
-            //print (response)
+            
+            guard let data = data else { return }
             
             do{
-                //let json = try JSONSerialization.jsonObject(with: data, options: [])
-        
-                //print(json)
                 let decoder = JSONDecoder()
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
                 self.userData = try decoder.decode(UserData.self, from: data)
@@ -84,7 +52,7 @@ class ViewController: UIViewController {
                 DispatchQueue.main.async {
                     self.nameLabel.text = self.userData?.data.firstName
                     self.emailLabel.text = self.userData?.data.email
- 
+                    
                     guard let imageURL = URL( string: (self.userData?.data.avatar)! ) else { return }
                     
                     URLSession.shared.dataTask(with: imageURL) { (data, response, error) in
@@ -96,14 +64,35 @@ class ViewController: UIViewController {
                         }
                     }.resume()
                 }
-                
             } catch {
                 print(error)
             }
         }.resume()
     }
     
-
+    @IBAction func rateUsButton(_ sender: Any) {
+        
+        guard let url = URL(string: "https://itunes.apple.com/app/id1054189818?action=write-review"), UIApplication.shared.canOpenURL(url) else { return }
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func ourPartnersButton(_ sender: Any) {
+        
+        guard let url = URL(string: "https://surprizeme.ru"), UIApplication.shared.canOpenURL(url) else  { return }
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func legalButton(_ sender: Any) {
+        
+        guard let url = URL(string: "https://srprsm.com/contacts/"), UIApplication.shared.canOpenURL(url) else { return }
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func mailClientButton(_ sender: Any) {
+        
+        guard let url = URL(string: "mailto:partners@surprizeme.ru"), UIApplication.shared.canOpenURL(url) else { return }
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
